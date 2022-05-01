803f74d1e2032260820cc2da371fdfab43f4f9b5
ksh <sosocom95@naver.com>
2022-05-01 22:54:16 +0900

    [SMST-10] 메인 화면 만들기 - 감정일기 달력 화면 제거 - 메인 화면에서 캘린더를 보고 기록 내용을 확인/추가하도록 UI 변경

--- app/src/main/java/app/sosocom/smallstep/ui/MainActivity.kt
+++ app/src/main/java/app/sosocom/smallstep/ui/MainActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import app.sosocom.smallstep.R
 import app.sosocom.smallstep.base.BaseActivity
 import app.sosocom.smallstep.databinding.ActivityMainBinding
-import app.sosocom.smallstep.ui.emotion_diary.EmotionCalendarActivity
+import app.sosocom.smallstep.ui.emotion_diary.EmotionDiaryActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun getLayoutResId() = R.layout.activity_main
@@ -28,7 +28,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     }
 
     fun onClickEmotionDiary(view: View) {
-        val intent = Intent(this, EmotionCalendarActivity::class.java)
+        val intent = Intent(this, EmotionDiaryActivity::class.java)
         startActivity(intent)
     }
 }
