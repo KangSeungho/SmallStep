f6dbbbd28e39bd1f218923b7e426b2c0ba035eb3
KangSeungho <sosocom95@naver.com>
2022-05-25 00:20:13 +0900

    [SMST-08] EmotionDiary 화면 만들기  - 등록/수정 화면 추가

--- app/src/main/java/app/sosocom/smallstep/ui/emotion_diary/DiaryEditActivity.kt
+++ app/src/main/java/app/sosocom/smallstep/ui/emotion_diary/DiaryEditActivity.kt
@@ -1,14 +1,42 @@
 package app.sosocom.smallstep.ui.emotion_diary
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import app.sosocom.smallstep.R
 import app.sosocom.smallstep.base.BaseActivity
 import app.sosocom.smallstep.databinding.ActivityDiaryEditBinding
+import app.sosocom.smallstep.model.Diary
+import app.sosocom.smallstep.util.ExtraConstants
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DiaryEditActivity : BaseActivity<ActivityDiaryEditBinding>(R.layout.activity_diary_edit) {
+    private val viewModel by viewModels<DiaryEditViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
+
+        loadData()
+    }
+
+    private fun loadData() {
+        val diary = intent.getParcelableExtra<Diary>(ExtraConstants.EXTRA_DIARY)
+
+        when(diary) {
+            // 등록
+            null -> {
+                viewModel.isRegister = true
+            }
+
+            // 수정
+            else -> {
+                viewModel.isRegister = false
+                viewModel.title.value = diary.title
+                viewModel.content.value = diary.content
+            }
+        }
     }
 }
