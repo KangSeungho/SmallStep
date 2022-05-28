c031a84c60875b4eb10dfa9bab7feec46b763113
KangSeungho <sosocom95@naver.com>
2022-05-28 22:07:02 +0900

    - buildSrc 추가

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,44 @@
+object Kotlin {
+    const val SDK = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21"
+}
+
+object AndroidX {
+    const val CORE = "androidx.core:core-ktx:1.7.0"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.4.1"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val MATERIAL = "com.google.android.material:material:1.6.0"
+
+    // Activity, Fragment 확장 함수 라이브러리
+    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:1.4.0"
+    const val FRAGMENT_KTX = "androidx.fragment:fragment-ktx:1.4.1"
+}
+
+object AndroidLifeCycle {
+    const val VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1"
+    const val LIVEDATA_KTX = "androidx.lifecycle:lifecycle-livedata-ktx:2.4.1"
+}
+
+object AndroidTest {
+    const val EXT_JUNIT = "androidx.test.ext:junit:1.1.3"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.4.0"
+}
+
+object DaggerHilt {
+    private const val version = "2.41"
+
+    const val PLUGIN = "com.google.dagger:hilt-android-gradle-plugin:$version"
+
+    const val SDK = "com.google.dagger:hilt-android:$version"
+    const val COMPILER = "com.google.dagger:hilt-android-compiler:$version"
+}
+
+object Glide {
+    private const val version = "4.13.0"
+
+    const val SDK = "com.github.bumptech.glide:glide:$version"
+    const val COMPILER = "com.github.bumptech.glide:compiler:$version"
+}
+
+object CalendarView {
+    const val APPLANDEO_SDK = "com.applandeo:material-calendar-view:1.7.0"
+}
