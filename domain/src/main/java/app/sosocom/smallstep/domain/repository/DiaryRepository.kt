39b70ad2787cfe88622a4945962765a53f0da7c0
KangSeungho <sosocom95@naver.com>
2022-05-31 22:49:50 +0900

    - DiaryRepository 추가

---
+++ domain/src/main/java/app/sosocom/smallstep/domain/repository/DiaryRepository.kt
@@ -0,0 +1,9 @@
+package app.sosocom.smallstep.domain.repository
+
+import app.sosocom.smallstep.domain.model.Diary
+
+interface DiaryRepository {
+    suspend fun insertDiary(diary: Diary)
+
+    suspend fun updateDiary(diary: Diary)
+}
