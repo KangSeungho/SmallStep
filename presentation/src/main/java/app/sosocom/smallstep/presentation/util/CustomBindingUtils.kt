85c4db6a90ab2aa60582207873745af5f02f3aec
ksh <sosocom95@gmail.com>
2022-06-08 22:32:55 +0900

    - change : 일기 쓰기/보기 화면에 날짜 표시

--- presentation/src/main/java/app/sosocom/smallstep/presentation/util/CustomBindingUtils.kt
+++ presentation/src/main/java/app/sosocom/smallstep/presentation/util/CustomBindingUtils.kt
@@ -1,15 +1,20 @@
 package app.sosocom.smallstep.presentation.util
 
 import android.view.View
+import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("android:visibleIf")
 fun View.setVisibleIf(visible: Boolean?) {
     isVisible = visible ?: false
 }
 
-@BindingAdapter("android:goneIf")
-fun View.setGoneIf(isGone: Boolean?) {
-    isVisible = !(isGone ?: false)
+@BindingAdapter("setFormatterDateText")
+fun setFormatterDateText(view: TextView, calendar: Calendar?) {
+    calendar ?: return
+
+    view.text = SimpleDateFormat("YYYY년 MM월 dd일 E요일", Locale.KOREA).format(calendar.time)
 }
