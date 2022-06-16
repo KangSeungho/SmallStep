b26634e1e6ca535f090fe45d8ceadcb6626c38b0
KangSeungho <sosocom95@naver.com>
2022-06-16 21:00:12 +0900

    - 일기 삭제 UseCase 연결

--- presentation/src/main/java/app/sosocom/smallstep/presentation/ui/diary/DiaryViewModel.kt
+++ presentation/src/main/java/app/sosocom/smallstep/presentation/ui/diary/DiaryViewModel.kt
@@ -4,9 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import app.sosocom.smallstep.domain.model.Diary
+import app.sosocom.smallstep.domain.usecase.DiaryDeleteUseCase
 import javax.inject.Inject
 
-class DiaryViewModel @Inject constructor(): ViewModel() {
+class DiaryViewModel @Inject constructor(
+    val diaryDeleteUseCase: DiaryDeleteUseCase
+): ViewModel() {
     private val _diary = MutableLiveData<Diary>()
     val diary: LiveData<Diary> = _diary
 
