3870b1e7292f4a26798c3166d1388529cb6de439
KangSeungho <sosocom95@naver.com>
2022-07-15 19:28:21 +0900

    - 메인 화면에서 행복점수 화면으로 이동 기능 추가

--- presentation/src/main/java/app/sosocom/smallstep/presentation/ui/happy_point/HappyPointListViewModel.kt
+++ presentation/src/main/java/app/sosocom/smallstep/presentation/ui/happy_point/HappyPointListViewModel.kt
@@ -5,9 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import app.sosocom.smallstep.domain.model.DailyHappyPointBundle
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HappyPointListViewModel : ViewModel() {
+class HappyPointListViewModel @Inject constructor(): ViewModel() {
     private val _dailyHappyPointBundle = MutableLiveData<DailyHappyPointBundle>()
     val dailyHappyPointBundle: LiveData<DailyHappyPointBundle> get() = _dailyHappyPointBundle
 
