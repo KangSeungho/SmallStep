a0086fb0d2ae2ec407ff30dbef3149f16605fd9b
KangSeungho <sosocom95@naver.com>
2022-07-19 22:40:57 +0900

    - Entity Nonnull 어노테이션 추가

--- data/src/main/java/app/sosocom/smallstep/data/entity/TodoEntity.kt
+++ data/src/main/java/app/sosocom/smallstep/data/entity/TodoEntity.kt
@@ -1,13 +1,14 @@
 package app.sosocom.smallstep.data.entity
 
+import androidx.annotation.NonNull
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "todo")
 data class TodoEntity(
     @PrimaryKey val id: Int? = null,    // ID
-    val content: String,                // 작업 내역
-    val isComplete: Boolean,            // 작업 완료 상태
-    val baseDate: Long,                 // 기준 날짜
-    val createdAt: Long                  // 작성 시각
+    @NonNull val content: String,       // 작업 내역
+    @NonNull val isComplete: Boolean,   // 작업 완료 상태
+    @NonNull val baseDate: Long,        // 기준 날짜
+    @NonNull val createdAt: Long        // 작성 시각
 )
