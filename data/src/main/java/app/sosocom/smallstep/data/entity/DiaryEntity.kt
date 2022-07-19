af8b593e37674298ecbff4f3ab897aab7a707246
KangSeungho <sosocom95@naver.com>
2022-07-19 22:40:57 +0900

    - Entity Nonnull 어노테이션 추가

--- data/src/main/java/app/sosocom/smallstep/data/entity/DiaryEntity.kt
+++ data/src/main/java/app/sosocom/smallstep/data/entity/DiaryEntity.kt
@@ -1,13 +1,14 @@
 package app.sosocom.smallstep.data.entity
 
+import androidx.annotation.NonNull
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "diary")
 data class DiaryEntity(
     @PrimaryKey val id: Int? = null,    // ID
-    val title: String,                  // 제목
-    val content: String,                // 내용
-    val baseDate: Long,                 // 기준 날짜
-    val createdAt: Long                 // 생성 시각 (저장 시간은 UTC-0, 실제 표시할 때는 사용자의 시간)
+    @NonNull val title: String,         // 제목
+    @NonNull val content: String,       // 내용
+    @NonNull val baseDate: Long,        // 기준 날짜
+    @NonNull val createdAt: Long        // 생성 시각 (저장 시간은 UTC-0, 실제 표시할 때는 사용자의 시간)
 )
--- data/src/main/java/app/sosocom/smallstep/data/entity/HappyPointEntity.kt
+++ data/src/main/java/app/sosocom/smallstep/data/entity/HappyPointEntity.kt
@@ -1,14 +1,15 @@
 package app.sosocom.smallstep.data.entity
 
+import androidx.annotation.NonNull
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "happy_point")
 data class HappyPointEntity(
     @PrimaryKey val id: Int? = null,        // ID
-    val content: String,                    // 행동
-    val comment: String,                    // 설명
-    val point: Float,                       // 점수 (0.0 ~ 5.0)
-    val baseDate: Long,                     // 기준 날짜
-    val createdAt: Long                      // 작성 시각
+    @NonNull val content: String,           // 행동
+    @NonNull val comment: String,           // 설명
+    @NonNull val point: Float,              // 점수 (0.0 ~ 5.0)
+    @NonNull val baseDate: Long,            // 기준 날짜
+    @NonNull val createdAt: Long            // 작성 시각
 )
