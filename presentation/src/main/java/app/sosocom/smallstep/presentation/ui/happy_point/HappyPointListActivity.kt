4d8cbddef1c1fa6e65f98f7c09e3a87f9151c477
KangSeungho <sosocom95@naver.com>
2022-07-20 18:16:21 +0900

    - 행복점수 리스트 화면에서 행복점수 추가 버튼 동작 처리 추가

--- presentation/src/main/java/app/sosocom/smallstep/presentation/ui/happy_point/HappyPointListActivity.kt
+++ presentation/src/main/java/app/sosocom/smallstep/presentation/ui/happy_point/HappyPointListActivity.kt
@@ -2,6 +2,7 @@ package app.sosocom.smallstep.presentation.ui.happy_point
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import app.sosocom.smallstep.domain.model.DailyHappyPointBundle
 import app.sosocom.smallstep.presentation.R
 import app.sosocom.smallstep.presentation.base.BaseActivity
@@ -9,12 +10,21 @@ import app.sosocom.smallstep.presentation.databinding.ActivityHappyPointListBind
 import app.sosocom.smallstep.presentation.ui.happy_point.adapter.HappyPointAdapter
 import app.sosocom.smallstep.presentation.util.ExtraConstants
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import java.time.LocalDate
 
 @AndroidEntryPoint
 class HappyPointListActivity : BaseActivity<ActivityHappyPointListBinding>(R.layout.activity_happy_point_list) {
     private val viewModel by viewModels<HappyPointListViewModel>()
 
     private val adapter = HappyPointAdapter()
+
+    private val editDialog by lazy {
+        HappyPointEditDialog(
+            context = this,
+            baseDate = viewModel.dailyHappyPointBundle.value?.baseDate ?: LocalDate.now()
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +62,18 @@ class HappyPointListActivity : BaseActivity<ActivityHappyPointListBinding>(R.lay
     private fun initUI() {
         // 리스트 세팅
         binding.listHappyPoint.adapter = adapter
+
+        // 아이템 추가 버튼
+        binding.btnAddHappyPoint.setOnClickListener {
+            editDialog.show()
+        }
+
+        // 작성 완료
+        editDialog.setOnDoneListener { happyPoint ->
+            lifecycleScope.launch {
+                viewModel.insertHappyPoint(happyPoint)
+            }
+        }
     }
 
     private fun initObserver() {
--- presentation/src/main/java/app/sosocom/smallstep/presentation/ui/happy_point/HappyPointListViewModel.kt
+++ presentation/src/main/java/app/sosocom/smallstep/presentation/ui/happy_point/HappyPointListViewModel.kt
@@ -4,15 +4,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import app.sosocom.smallstep.domain.model.DailyHappyPointBundle
+import app.sosocom.smallstep.domain.model.HappyPoint
+import app.sosocom.smallstep.domain.usecase.HappyPointInsertUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HappyPointListViewModel @Inject constructor(): ViewModel() {
+class HappyPointListViewModel @Inject constructor(
+    private val insertUseCase: HappyPointInsertUseCase
+): ViewModel() {
     private val _dailyHappyPointBundle = MutableLiveData<DailyHappyPointBundle>()
     val dailyHappyPointBundle: LiveData<DailyHappyPointBundle> get() = _dailyHappyPointBundle
 
     fun setDailyHappyPointBundle(bundle: DailyHappyPointBundle) {
         _dailyHappyPointBundle.value = bundle
     }
+
+    suspend fun insertHappyPoint(happyPoint: HappyPoint) = insertUseCase(happyPoint)
 }
