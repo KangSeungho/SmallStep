42e9a18b52a7810eb3bd9f62b13a481828fa5fa8
ksh <sosocom95@gmail.com>
2022-07-22 00:02:43 +0900

    - Entity 이름 지정

--- data/src/main/java/app/sosocom/smallstep/data/entity/DiaryEntity.kt
+++ data/src/main/java/app/sosocom/smallstep/data/entity/DiaryEntity.kt
@@ -7,9 +7,9 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "diary")
 data class DiaryEntity(
-    @PrimaryKey val id: Int? = null,    // ID
-    @ColumnInfo(name = "title") @NonNull val title: String,         // 제목
-    @ColumnInfo(name = "content") @NonNull val content: String,       // 내용
-    @ColumnInfo(name = "base_date") @NonNull val baseDate: Long,        // 기준 날짜
-    @ColumnInfo(name = "create_at") @NonNull val createdAt: Long        // 생성 시각 (저장 시간은 UTC-0, 실제 표시할 때는 사용자의 시간)
+    @PrimaryKey                                 val id: Int? = null,    // ID
+    @ColumnInfo(name = "title") @NonNull        val title: String,      // 제목
+    @ColumnInfo(name = "content") @NonNull      val content: String,    // 내용
+    @ColumnInfo(name = "base_date") @NonNull    val baseDate: Long,     // 기준 날짜
+    @ColumnInfo(name = "create_at") @NonNull    val createdAt: Long     // 생성 시각 (저장 시간은 UTC-0, 실제 표시할 때는 사용자의 시간)
 )
--- data/src/main/java/app/sosocom/smallstep/data/entity/HappyPointEntity.kt
+++ data/src/main/java/app/sosocom/smallstep/data/entity/HappyPointEntity.kt
@@ -1,15 +1,16 @@
 package app.sosocom.smallstep.data.entity
 
 import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "happy_point")
 data class HappyPointEntity(
-    @PrimaryKey val id: Int? = null,        // ID
-    @NonNull val content: String,           // 행동
-    @NonNull val comment: String,           // 설명
-    @NonNull val point: Float,              // 점수 (0.0 ~ 5.0)
-    @NonNull val baseDate: Long,            // 기준 날짜
-    @NonNull val createdAt: Long            // 작성 시각
+    @PrimaryKey                                 val id: Int? = null,           // ID
+    @ColumnInfo(name = "content") @NonNull      val content: String,           // 행동
+    @ColumnInfo(name = "comment") @NonNull      val comment: String,           // 설명
+    @ColumnInfo(name = "point") @NonNull        val point: Float,              // 점수 (0.0 ~ 5.0)
+    @ColumnInfo(name = "base_date") @NonNull    val baseDate: Long,            // 기준 날짜
+    @ColumnInfo(name = "created_at") @NonNull   val createdAt: Long            // 작성 시각
 )
