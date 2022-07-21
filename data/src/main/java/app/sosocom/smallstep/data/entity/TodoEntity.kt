cef42c5da7c2ea0633e0c6f3c293394fa85e8ac8
ksh <sosocom95@gmail.com>
2022-07-22 00:02:43 +0900

    - Entity 이름 지정

--- data/src/main/java/app/sosocom/smallstep/data/entity/TodoEntity.kt
+++ data/src/main/java/app/sosocom/smallstep/data/entity/TodoEntity.kt
@@ -1,14 +1,15 @@
 package app.sosocom.smallstep.data.entity
 
 import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "todo")
 data class TodoEntity(
-    @PrimaryKey val id: Int? = null,    // ID
-    @NonNull val content: String,       // 작업 내역
-    @NonNull val isComplete: Boolean,   // 작업 완료 상태
-    @NonNull val baseDate: Long,        // 기준 날짜
-    @NonNull val createdAt: Long        // 작성 시각
+    @PrimaryKey                                 val id: Int? = null,       // ID
+    @ColumnInfo(name = "content") @NonNull      val content: String,       // 작업 내역
+    @ColumnInfo(name = "is_complete") @NonNull  val isComplete: Boolean,   // 작업 완료 상태
+    @ColumnInfo(name = "base_date") @NonNull    val baseDate: Long,        // 기준 날짜
+    @ColumnInfo(name = "create_at") @NonNull    val createdAt: Long        // 작성 시각
 )
